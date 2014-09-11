0e5a10e4b659a697a6f3beec2a97a9f4d4bc9e02
Kevin Butler <haqkrs@gmail.com>
2014-09-11 01:17:25 +0100

    Consume and handle server response.

--- src/socks4a.rs
+++ src/socks4a.rs
@@ -1,4 +1,5 @@
-use std::io::{IoResult, TcpStream};
+use std::io::{IoResult, IoError, IoErrorKind, TcpStream, ConnectionRefused,
+							ConnectionFailed, OtherIoError};
 
 pub struct Socks4a {
 	sockshost: String,
@@ -28,6 +29,33 @@ impl Socks4a {
 		try!(stream.write_str(self.host.as_slice()));
 		try!(stream.write([0x00]));
 
-		Ok(stream)
+		// read null byte
+		if 0 != try!(stream.read_u8()) {
+			return io_err(OtherIoError, "Expected null byte not found");
+		}
+
+		// read status
+		match try!(stream.read_u8()) {
+			// request granted
+			0x5a => {
+				let _port = try!(stream.read_be_u16());
+				let _ip = try!(stream.read_be_u32());
+
+				Ok(stream)
+			}
+			// request rejected or failed
+			0x5b => io_err(ConnectionRefused, "Request rejected or failed"),
+			// request failed because client is not running identd (or unreachable)
+			0x5c => io_err(ConnectionFailed, "Client unreachable"),
+			// request failed because client's identd could not confirm the user ID
+			// string in the request
+			0x5d => io_err(ConnectionRefused, "Unknown user"),
+			x => fail!("Unexpected status byte: {}", x)
+		}
 	}
 }
+
+fn io_err<T>(kind: IoErrorKind, desc: &'static str) -> IoResult<T> {
+	Err(IoError { kind: kind, desc: desc, detail: None })
+}
+
