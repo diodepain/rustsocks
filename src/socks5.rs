fcd9bff1fbcaa6a918e8baa6e60fcad9600f1685
Kevin Butler <haqkrs@gmail.com>
2014-09-13 14:41:01 +0100

    Accept Domain/Ipv4/Ipv6 destinations.

--- src/socks5.rs
+++ src/socks5.rs
@@ -1,9 +1,42 @@
 use std::io::{IoResult, IoError, IoErrorKind, TcpStream, ConnectionRefused,
               ConnectionFailed, OtherIoError};
+use std::io::net::ip::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 enum AuthMethod<'s> {
     NoAuth,
     UPass(&'s str, &'s str)
+}
+
+pub trait Socks5Destination {
+    fn write_destination(&self, &mut TcpStream) -> IoResult<()>;
+}
+
+impl<'a> Socks5Destination for &'a str {
+    fn write_destination(&self, stream: &mut TcpStream) -> IoResult<()> {
+        if self.len() > 255 {
+            return io_err(OtherIoError, "Domain length is too long");
+        }
+
+        try!(stream.write([0x03, self.len() as u8]));
+        try!(stream.write_str(*self));
+        Ok(())
+    }
+}
+
+impl<'a> Socks5Destination for IpAddr {
+    fn write_destination(&self, stream: &mut TcpStream) -> IoResult<()> {
+        match *self {
+            Ipv4Addr(a, b, c, d) => try!(stream.write([0x01, a, b, c, d])),
+            Ipv6Addr(a, b, c, d, e, f, g, h) => {
+                try!(stream.write_u8(0x04));
+                for &pair in [a, b, c, d, e, f, g, h].iter() {
+                    try!(stream.write_be_u16(pair));
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
 
 pub struct Socks5<'a> {
@@ -25,7 +58,8 @@ impl<'a> Socks5<'a> {
         self.socks_auth = UPass(uname, passwd);
     }
 
-    pub fn connect(&mut self, host: &str, port: u16) -> IoResult<TcpStream> {
+    pub fn connect<T: Socks5Destination>(&mut self, destination: T, port: u16)
+                                                        -> IoResult<TcpStream> {
         let mut stream = try!(TcpStream::connect(self.socks_host, self.socks_port));
         try!(stream.write([0x05u8]));
         match self.socks_auth {
@@ -73,8 +107,8 @@ impl<'a> Socks5<'a> {
                     "Wrong authentication method from server"); }
         }
 
-        try!(stream.write([0x05u8, 0x01, 0x00, 0x03, host.len() as u8]));
-        try!(stream.write_str(host));
+        try!(stream.write([0x05u8, 0x01, 0x00]));
+        try!(destination.write_destination(&mut stream));
         try!(stream.write_be_u16(port));
 
         if try!(stream.read_u8()) != 0x05 {
